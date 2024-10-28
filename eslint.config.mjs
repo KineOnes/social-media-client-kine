5fd7f6f4b8624b7064dab6abc97921f493f814a9
Kine Ones <kineones@Kines-MacBook-Air.local>
2024-10-28 11:09:41 +0100

    Change eslint sourscetype from script to module

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module", // Enables ES module syntax (import/export)
+      ecmaVersion: 2020, // Sets ECMAScript version to 2020 for modern features
+      globals: globals.browser, // Adds browser globals (like window, document, etc.)
+    },
+  },
   pluginJs.configs.recommended,
 ];
