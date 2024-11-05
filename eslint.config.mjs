9e63ba1c90d3357cba98517e30b69d920f482363
Kine Ones <kineones@Kines-MacBook-Air.local>
2024-11-05 18:08:51 +0100

    adjust content in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,38 +9,27 @@ export default [
     files: ["**/*.test.js", "**/*.spec.js"],
     languageOptions: {
       globals: {
-        ...Object.fromEntries(Object.keys(globals.browser).map(key => [key, "readonly"])),
-        ...Object.fromEntries(Object.keys(globals.node).map(key => [key, "readonly"])),
-        ...Object.fromEntries(Object.keys(jest.environments.globals).map(key => [key, "readonly"])),
+        ...globals.browser,
+        ...globals.node,
+        ...jest.environments.globals,
       },
-    },
-    plugins: {
-      jest,
-    },
-    rules: {
-      ...jest.configs.recommended.rules,
     },
   },
   {
     files: ["**/*.cy.js"],
-    plugins: {
-      cypress,
-    },
+    plugins: [cypress],
     languageOptions: {
       globals: {
-        ...Object.fromEntries(Object.keys(globals.browser).map(key => [key, "readonly"])),
-        ...Object.fromEntries(Object.keys(globals.node).map(key => [key, "readonly"])),
-        ...Object.fromEntries(Object.keys(cypress.environments.globals).map(key => [key, "readonly"])),
+        ...globals.browser,
+        ...globals.node,
+        ...cypress.environments.globals,
       },
-    },
-    rules: {
-      ...cypress.configs.recommended.rules,
     },
   },
   {
     files: ["cypress.config.js"],
-    languageOptions: {
-      globals: Object.fromEntries(Object.keys(globals.node).map(key => [key, "readonly"])),
+    env: {
+      node: true,
     },
     parserOptions: {
       sourceType: "commonjs",
